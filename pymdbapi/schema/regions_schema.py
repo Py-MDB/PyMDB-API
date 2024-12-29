727840a9f7cc23a52dea6995f3f320fa4744bca4
jesse_b <jesse.butryn@gmail.com>
2024-12-29 10:14:56 -0600

    added regions and ability to link facilities to regions

--- pymdbapi/schema/regions_schema.py
+++ pymdbapi/schema/regions_schema.py
@@ -1,16 +1,18 @@
-facilities_schema = {
+regions_schema = {
     'id': {'type': 'string', 'required': False},
     'name': {'type': 'string', 'required': True},
     'slug': {'type': 'string', 'required': True},
     'location': {'type': 'string', 'required': True},
     'created': {'type': 'string', 'required': False},
-    'region': {
-        'type': 'dict',
+    'facilities': {
+        'type': 'list',
         'schema': {
-            'href': {'type': 'string', 'required': False},
-            'id': {'type': 'string', 'required': False},
-        }
+            'type': 'dict',
+            'schema': {
+                'href': {'type': 'string', 'required': False},
+                'id': {'type': 'string', 'required': False},
+            }
+        }   
     },
-    'rack_capacity': {'type': 'integer', 'required': True},
     'state': {'type': 'string', 'required': True},
 }
